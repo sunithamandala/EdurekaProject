d012a785e541cf7061180e9a494aaf99e35b758e
sunithajadal <sunithajadal@LAPTOP-24LVESBU>
2018-08-28 14:07:52 -0700

    Adding Day1 Code

---
+++ EdurekaProject/src/day1/HelloWorld.java
@@ -0,0 +1,15 @@
+package day1;
+
+/**
+ * @author sunithajadal
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World again");
+
+	}
+
+}

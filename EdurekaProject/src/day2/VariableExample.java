fe2006afbff5607be9c47630562c78f3a4f6183d
sunithajadal <sunithajadal@LAPTOP-24LVESBU>
2018-08-28 15:54:15 -0700

    Day 2 Variable Example adding

---
+++ EdurekaProject/src/day2/VariableExample.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int firstNumber;
+		firstNumber= 20;
+		int secondNumber = 2000;
+
+		
+		System.out.println("First Number:" +firstNumber);
+		System.out.println("Second Number : " +secondNumber);
+		System.out.println("Number : " +firstNumber + secondNumber);
+		System.out.println("Number : " +(firstNumber + secondNumber));
+
+	}
+
+}
